026f6c4d36f6cac6eb90e87420fc43274983a0cf
Pluto <pluotio@duck.com>
2026-04-07 14:19:46 -0400

    Refactor main.py and add new commands

--- main.py
+++ main.py
@@ -7,13 +7,10 @@ import os
 import random
 import giphy_client
 from giphy_client.rest import ApiException
-
-
 load_dotenv()
 token = os.getenv('token')
 api_key = os.getenv('api_key')
 bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
@@ -26,23 +23,19 @@ async def on_ready():
 @bot.tree.command(name = 'ping')
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f'pong')
-
 #Heads or tails
 @bot.tree.command(name = 'coinflip')
 async def coinflip(interaction: discord.Interaction):
     o = ['Heads', 'Tails']
     output = random.choice(o)
     await interaction.response.send_message(f'The coin landed on {output}')
-
-
 #send astolfo pic :3
 @bot.tree.command(name = 'astolfo')
 async def astolfo(interaction: discord.Interaction):
     await interaction.response.send_message(f'http://fateextellalink.com/characters/astolfo/images/astolfo.png')
-
 #uhnime
 @bot.command(name = 'gif')
-async def gif(ctx,*,q="cat"):
+async def gif(ctx,*,q="Anime"):
     api_instance = giphy_client.DefaultApi()
     try:
         api_response = api_instance.gifs_search_get(api_key, q, limit=5, rating='g')
@@ -55,7 +48,6 @@ async def gif(ctx,*,q="cat"):
         await ctx.channel.send(embed=embed)
     except ApiException as r:
         print("Exception from api")
-
 #uhnime / command
 @bot.tree.command(name='gif')
 async def gif(interaction: discord.Interaction, q: str = "catgirl"):
@@ -64,14 +56,17 @@ async def gif(interaction: discord.Interaction, q: str = "catgirl"):
         api_response = api_instance.gifs_search_get(api_key, q, limit=5, rating='g')
         lst = list(api_response.data)
         giff = random.choice(lst)
-
         embed = discord.Embed(title=q)
         embed.set_image(url=f"https://media.giphy.com/media/{giff.id}/giphy.gif")
-
         await interaction.response.send_message(embed=embed)
     except ApiException as r:
         print("Exception from api")
-
-
+#rawr
+@bot.tree.command(name = 'rawr')
+async def rawr(interaction: discord.Interaction):
+    await interaction.response.send_message(f'*rawr~*')
+@bot.tree.command(name = 'meow')
+async def meow(interaction: discord.Interaction):
+    await interaction.response.send_message(f'meow :3')
 #start bot
 bot.run(token)
